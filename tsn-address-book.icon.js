7a74c76471c1c414cdde1acd69b78ec0519a7041
armin37 <33215109+armin37@users.noreply.github.com>
2022-11-19 11:59:42 +0330

    Add files via upload

---
+++ tsn-address-book.icon.js
@@ -0,0 +1,30 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'fas';
+var iconName = 'address_book';
+var width = 32;
+var height = 32;
+var aliases = [128657,"address-book"];
+var unicode = 'f0f9';
+var svgPathData = 'M0 4h4v20H0zm6 0h2v20H6zm4 0h2v20h-2zm6 0h2v20h-2zm8 0h2v20h-2zm6 0h2v20h-2zM20 4h1v20h-1zm-6 0h1v20h-1zm13 0h1v20h-1zM0 26h2v2H0zm6 0h2v2H6zm4 0h2v2h-2zm10 0h2v2h-2zm10 0h2v2h-2zm-6 0h4v2h-4zm-10 0h4v2h-4z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.tsnAddressBook = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
